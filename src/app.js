3b370e3bd00390d360321985674051b05f3a064a
Jaidev Bangar <solaxes@gmail.com>
2025-04-17 18:48:03 +0530

    Route updated

--- src/app.js
+++ src/app.js
@@ -1,35 +1,17 @@
 const express = require('express');
 
 const app = express();
-app.use('/about', (req, res) => {
-    res.send("This is about us page");
-    return;
-});
-app.use('/home', (req, res) => {
-    res.send('Welcome to the home page');
-    return;
-});
-app.use('/contact', (req, res) => {
-    res.send('Welcome to the contact page');
-    return;
+
+app.get("/api/users", (req, res) => {
+  res.json([
+    { id: 1, name: "John Doe" },
+    { id: 2, name: "Jane Doe" },
+  ]);
 });
 
-app.use("/api/users", (req, res) => {
-    res.json([
-        { id: 1, name: 'John Doe' },
-        { id: 2, name: 'Jane Doe' }
-    ]);
-    return;
+app.post("/api/users", (req, res) => {
+  res.send("Data successfully saved to the database");
 });
-
-app.use((req, res) => {
-    res.send('Hello World!');
-    return;
-});
-
-
-
-
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
